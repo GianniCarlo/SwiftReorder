1d44016b8227000304978635a6d4302c1dd0d5bc
Gianni Carlo <gcarlo89@hotmail.com>
2018-04-29 13:22:20 -0500

    Replace project with framework base for carthage

---
+++ SwiftReorder/SwiftReorder.h
@@ -0,0 +1,19 @@
+//
+//  SwiftReorder.h
+//  SwiftReorder
+//
+//  Created by Gianni Carlo on 4/29/18.
+//  Copyright © 2018 Tortuga Power. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftReorder.
+FOUNDATION_EXPORT double SwiftReorderVersionNumber;
+
+//! Project version string for SwiftReorder.
+FOUNDATION_EXPORT const unsigned char SwiftReorderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftReorder/PublicHeader.h>
+
+
